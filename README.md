6d24dc77b2eb644ee8c9effeed12c363534e17fa
Wish <breezewish@outlook.com>
2024-04-19 10:51:43 +0800

    Initial impl
    Signed-off-by: Wish <breezewish@outlook.com>

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # x86-instruction-set-analyzer
-Analyze x86 instruction sets for a binary
+
+Analyze x86 instruction sets for a binary.
+
+## Example
+
+```shell
+$ cargo run -- ../rust-rocksdb/target/release/rocksdb
+
+Format: Elf Little-endian 64-bit
+Kind: Dynamic
+Architecture: X86_64
+
+Instruction set usage:
+X64: 1004745 (71.87%)
+INTEL8086: 154738 (11.07%)
+INTEL386: 149737 (10.71%)
+AVX: 48527 (3.47%)
+MULTIBYTENOP: 12733 (0.91%)
+CMOV: 11046 (0.79%)
+BMI2: 4477 (0.32%)
+AVX512F: 3121 (0.22%)
+SSE: 2853 (0.20%)
+AVX2: 2254 (0.16%)
+AVX512VL: 2084 (0.15%)
+BMI1: 1100 (0.08%)
+AVX512BW: 634 (0.05%)
+LZCNT: 555 (0.04%)
+POPCNT: 494 (0.04%)
+SSE2: 375 (0.03%)
+AVX512DQ: 140 (0.01%)
+INTEL186: 123 (0.01%)
+INTEL486: 89 (0.01%)
+INTEL286: 87 (0.01%)
+FPU: 80 (0.01%)
+FMA: 40 (0.00%)
+PAUSE: 14 (0.00%)
+MOVBE: 13 (0.00%)
+CPUID: 10 (0.00%)
+TSC: 6 (0.00%)
+AVX512CD: 4 (0.00%)
+CLFSH: 4 (0.00%)
+RDRAND: 1 (0.00%)
+RDSEED: 1 (0.00%)
+XSAVE: 1 (0.00%)
+```
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use object::{Object, ObjectSection};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let binary_data = std::fs::read(&args[1])?;
+    let file = object::File::parse(&*binary_data)?;
+
+    println!(
+        "Format: {:?} {:?}-endian {}-bit",
+        file.format(),
+        file.endianness(),
+        if file.is_64() { "64" } else { "32" }
+    );
+    println!("Kind: {:?}", file.kind());
+    println!("Architecture: {:?}", file.architecture());
+    if let Some(sub_architecture) = file.sub_architecture() {
+        println!("Sub-Architecture: {:?}", sub_architecture);
+    }
+
+    let bitness = match file.architecture() {
+        object::Architecture::X86_64 => 64,
+        object::Architecture::X86_64_X32 => 32,
+        object::Architecture::I386 => 32,
+        _ => {
+            eprintln!("Unsupported architecture: {:?}", file.architecture());
+            std::process::exit(1);
+        }
+    };
+
+    let mut iset_counter = vec![0usize; 256];
+    let mut total_ins = 0;
+
+    for section in file.sections() {
+        if section.kind() != object::SectionKind::Text {
+            continue;
+        }
+
+        let data = section.data()?;
+        let decoder =
+            iced_x86::Decoder::new(bitness, data, iced_x86::DecoderOptions::NO_INVALID_CHECK);
+        for instruction in decoder {
+            if instruction.is_invalid() {
+                continue;
+            }
+            for &feature in instruction.cpuid_features() {
+                iset_counter[feature as usize] += 1;
+            }
+            total_ins += 1;
+        }
+    }
+
+    // Sort by count
+    let mut ordered_iset_counter = iset_counter
+        .into_iter()
+        .enumerate()
+        .filter(|&(_, count)| count != 0)
+        .collect::<Vec<_>>();
+
+    println!();
+    println!("Instruction set usage:");
+    ordered_iset_counter.sort_by(|a, b| b.1.cmp(&a.1));
+    for (iset, count) in ordered_iset_counter.iter() {
+        let iset_e = iced_x86::CpuidFeature::try_from(*iset).unwrap();
+        let percent = (*count as f64 / total_ins as f64) * 100.0;
+        println!("{:?}: {} ({:.2}%)", iset_e, count, percent);
+    }
+
+    Ok(())
+}
